95fbe06e00f1c1d65baadfe51bd6a71587214862
Aksad Hamirani <aksadhamirani99@gmail.com>
2021-02-05 09:21:29 +0530

    Add files via upload

---
+++ flask_api.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from flask import Flask ,request
+import pandas as pd
+import numpy as np
+import pickle
+
+app=Flask(__name__)
+pickle_in=open('classifier.pkl','rb')
+classifier=pickle.load(pickle_in)
+
+@app.route('/')
+def welcome():
+    return 'hello aksad'
+
+
+@app.route('/predict')
+def predict_note_authentication():
+    variance=request.args.get('variance')
+    skewness=request.args.get('skewness')
+    curtosis=request.args.get('curtosis')
+    entropy=request.args.get('entropy')
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    return 'the prediction value is '+str(prediction)
+
+@app.route('/predict_file',methods=['POST'])
+def predict_note_file():
+    df_test=pd.read_csv(request.files.get('file'))
+    prediction=classifier.predict(df_test)
+    return'the predicted value for csv  is '+str(list(prediction))
+
+
+if __name__=='__main__':
+    app.run()
+    
+
+
